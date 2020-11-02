52ef33528e5a1b1e3f19a6742c41d6c054c81190
swetank soni <iamswetanksoni@gmail.com>
2020-11-02 13:06:06 +0530

    add multi-env-config , requirement.txt

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ mkdir users && cd users
 mkdir project
 python3.6 -m venv env
 source env/bin/activate
+deactivate
+
+## Dependencies - requirement.txt
+
 (env)$ pip install flask==0.12.2
 (env)$ pip install Werkzeug==0.16.1
--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -2,6 +2,10 @@
 from flask import Flask, jsonify
 # instantiate the app
 app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
 @app.route('/users/ping', methods=['GET'])
 def ping_pong(): return jsonify({
         'status': 'success',
