08cca58e7a58198d93b82c932733f73076835fc1
swetank soni <iamswetanksoni@gmail.com>
2020-11-02 12:53:24 +0530

    create sample ping-pong route

---
+++ README.md
@@ -0,0 +1,9 @@
+### Getting Started
+
+mkdir testdriven-app && cd testdriven-app $ mkdir services && cd services
+mkdir users && cd users
+mkdir project
+python3.6 -m venv env
+source env/bin/activate
+(env)$ pip install flask==0.12.2
+(env)$ pip install Werkzeug==0.16.1
---
+++ testdriven-app/services/users/project/__init__.py
@@ -0,0 +1,9 @@
+# services/users/project/__init__.py
+from flask import Flask, jsonify
+# instantiate the app
+app = Flask(__name__)
+@app.route('/users/ping', methods=['GET'])
+def ping_pong(): return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
