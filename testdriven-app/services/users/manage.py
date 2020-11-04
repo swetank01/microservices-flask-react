0f2a01c165a91d9536426905393f8290c313a40c
swetank soni <iamswetanksoni@gmail.com>
2020-11-04 18:17:51 +0530

    add cli command -> recreate-db

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,6 +1,15 @@
 # services/users/manage.py
 from flask.cli import FlaskGroup 
-from project import app
+from project import app, db
 
 cli = FlaskGroup(app)
-if __name__ == '__main__': cli()
+
+@cli.command()
+
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == '__main__': 
+    cli()
