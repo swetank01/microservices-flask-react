b574f0192273cb49e89a8654cd757f0ac5d03332
swetank soni <iamswetanksoni@gmail.com>
2020-11-05 15:05:17 +0530

    add test cli and install Flask-Testing==0.6.2 dependencies

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,6 +1,7 @@
 # services/users/manage.py
 from flask.cli import FlaskGroup 
 from project import app, db
+import unittest
 
 cli = FlaskGroup(app)
 
@@ -10,6 +11,16 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """ Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py') 
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__': 
     cli()
